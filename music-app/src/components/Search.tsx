7197596179d60621b481c464ba39aa0f93eb1fd4
Bubul <bubul@Bubuls-MBP.Dlink>
2022-06-14 15:45:51 +0530

    finished basic structure of search component

--- music-app/src/components/Search.tsx
+++ music-app/src/components/Search.tsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import { FormEvent, useState } from 'react';
+import './Search.css'
 
+type SearchProps = {
+
+}
 const Search = () => {
+    const [songSearch, setSongSearch] = useState('');
+    const changeSong = (e: FormEvent<HTMLInputElement>) => {
+        setSongSearch(e.currentTarget.value)
+    }
     return (
-        <div>
-            <h2>Search Music</h2>
-            <div className='container'>
+        <div className='header'>
+            <h4>Search Music</h4>
+            
                 <div className="row">
                     <form className="offset-s3 col s6">
                         <div className="input-field col s12">
-                            <input id="email" type="email" className="validate"/>
-                            <label htmlFor="email">Search Songs</label>
+                            
+                            <input id="email" type="email" className="validate" value={songSearch}
+                            onChange={changeSong} placeholder="Search Songs"/>
+                            
+                            <i className="material-icons prefix">search</i>
                         </div>
                     </form>
                 </div>
-            </div>
         </div>
     );
 };
