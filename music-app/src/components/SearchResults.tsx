9594f6cfc30c50f48e2719030dab29b92b887cfb
Bubul <bubul@Bubuls-MBP.Dlink>
2022-06-15 12:54:05 +0530

    Revert "scroll fix for standard resolution"
    This reverts commit 132a3a8975b0cc9d59c75acfc4efa3f21ab3892f.

--- music-app/src/components/SearchResults.tsx
+++ music-app/src/components/SearchResults.tsx
@@ -9,48 +9,41 @@ type ShowSongsProps = {
     sendRequest: (search:string, pageNo: number) => void
 }
 const SearchResults: React.FC<ShowSongsProps> = ({songs, sendRequest}) => {
-    let songDetails;
     const songSearch = useSelector((state:SongDetailsState) => state.searchString, shallowEqual) || '';
     const pageNo = useSelector((state:SongDetailsState) => state.pageNo, shallowEqual);
-    
     const loadMoreData = (e: FormEvent) => {
         e.preventDefault();
         const bottom = e.currentTarget.scrollHeight - e.currentTarget.scrollTop
-         <= e.currentTarget.clientHeight + 5;
+         === e.currentTarget.clientHeight;
         if(bottom){
             let newPageNo: number = pageNo + 1;
             sendRequest(songSearch, newPageNo);
         }
     }
-    if(songs.length > 0){
-        songDetails =  <table>
-        <thead>
-        <tr>
-            <th>Song</th>
-            <th>Artist</th>
-            <th>Album</th>
-        </tr>
-        </thead>
-
-        <tbody>
-            {
-                songs.map(song => (
-                    <tr key={song._id}>
-                        <td>{song.song}</td>
-                        <td>{song.artist}</td>
-                        <td>{song.album}</td>
-                    </tr>
-                ))
-            }
-        </tbody>
-    </table>
-    } else {
-        songDetails = <h4>No Songs Match Your Search</h4>
-    }
     return (
         <>
             <div className='songs-table' onScroll={loadMoreData}>
-                {songDetails}
+            <table>
+                <thead>
+                <tr>
+                    <th>Song</th>
+                    <th>Artist</th>
+                    <th>Album</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                    {
+                        songs.map(song => (
+                            <tr key={song._id}>
+                                <td>{song.song}</td>
+                                <td>{song.artist}</td>
+                                <td>{song.album}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
             </div>
            
         </>
